855b877acbf5d759daca871825b82e96c4364b83
Calaverd <bfxdosval@gmail.com>
2021-02-02 01:54:26 -0600

    Actulizados datos covid, haciendo otros test con echarts

---
+++ assets/datos_ratitas/graficas_ratitas.js
@@ -0,0 +1,215 @@
+const MESES = ["Enero","Febrero","Marzo","Abril","Mayo","Junio","Julio","Agosto",
+"Septiembre","Octubre","Noviembre","Diciembre"];
+const DIAS_S = ["Domingo","Lunes","Martes","Miércoles","Jueves","Viernes","Sábado"]
+const base_date_obj = new Date();
+
+let lista_de_municipios = [];
+let diccionario_municipios = {};
+let HistoricoDefunciones = undefined;
+let HistoricoContagios = undefined;
+let MaximosContagiosMunicipio = 0;
+let MaximosDefuncionesMunicipio;
+
+const file_directory = '/assets/datos_ratitas/';
+
+/* Start Up data... */
+const request_data = new Request(file_directory+'ratitas.json');
+
+
+const promesa = fetch(request_data).then(
+    function(response) {
+        return response.json();
+    }).then(function(data) {
+
+
+        let grupo_1 = data['grupo_1'];
+        let grupo_2 = data['grupo_2'];
+
+        let grupo_1_peso = [];
+        let grupo_1_kmeans = [];
+        let grupo_1_g120 = [];
+        let grupo_1_tag = [];
+        let grupo_1_insul = [];
+        
+        let grupo_2_peso = [];
+        let grupo_2_kmeans = [];
+        let grupo_2_g120 = [];
+        let grupo_2_tag = [];
+        let grupo_2_insul = [];
+        
+        
+
+
+        grupo_1.forEach(element => {
+            grupo_1_peso.push(element['S20']);
+            grupo_1_kmeans.push(element['kmeas']);
+            grupo_1_g120.push(element['G120']);
+            grupo_1_tag.push(element['TAG']);
+            grupo_1_insul.push(element['INS']);
+        });
+
+        grupo_2.forEach(element => {
+            grupo_2_peso.push(element['S20']);
+            grupo_2_kmeans.push(element['kmeas']);
+            grupo_2_g120.push(element['G120']);
+            grupo_2_tag.push(element['TAG']);
+            grupo_2_insul.push(element['INS']);
+        });
+
+
+        
+        let OptionChart1 = {
+            title: {
+                text: 'Peso vs Kmeans',
+                left: 'center'
+            },
+            xAxis: {
+                type: 'category',
+                data: ['kmeans 1','kmeans 2']
+            },
+            tooltip: {
+                trigger: 'item',
+                axisPointer: {
+                    type: 'shadow'
+                }
+            },
+            yAxis: {
+                type: 'value',
+                name: 'Gramos',
+                min: 400,
+            },
+            series: [{
+                type: 'boxplot',
+                scale: 'log',
+                data: [
+                    grupo_1_peso,
+                    grupo_2_peso
+                    ]
+                }
+            ]
+        };
+
+        let OptionChart2 = {
+            title: {
+                text: 'G120 vs Kmeans',
+                left: 'center'
+            },
+            xAxis: {
+                type: 'category',
+                data: ['kmeans 1','kmeans 2']
+            },
+            tooltip: {
+                trigger: 'item',
+                axisPointer: {
+                    type: 'shadow'
+                }
+            },
+            yAxis: {
+                type: 'value',
+                name: 'Glucosa',
+                min: 50,
+            },
+            series: [{
+                type: 'boxplot',
+                scale: 'log',
+                data: [
+                    grupo_1_g120,
+                    grupo_2_g120
+                    ]
+                }
+            ]
+        };
+
+        let OptionChart3 = {
+            title: {
+                text: 'TAG vs Kmeans',
+                left: 'center'
+            },
+            xAxis: {
+                type: 'category',
+                data: ['kmeans 1','kmeans 2']
+            },
+            tooltip: {
+                trigger: 'item',
+                axisPointer: {
+                    type: 'shadow'
+                }
+            },
+            yAxis: {
+                type: 'value',
+                name: 'Triglicéridos',
+            },
+            series: [{
+                name: 'TAG vs Kmeans',
+                type: 'boxplot',
+                scale: 'log',
+                data: [
+                    grupo_1_tag,
+                    grupo_2_tag
+                    ]
+                }
+            ]
+        };
+
+        let OptionChart4 = {
+            legend: {
+                data: ['Grupo 1', 'Grupo 2'],
+                //left: '10',
+                //bottom: '5%'
+                //orient: 'vertical'
+                textStyle:{
+                    fontSize : 17,
+                    fontStyle : 'bold',
+                    fontFamily: 'Atkinson'
+                    }
+                },
+            title: {
+                text: 'TAG vs Insulina',
+                left: 'center'
+            },
+            xAxis: {
+                name:'Insulina',
+                type: 'category',
+                data: grupo_1_insul
+            },
+            tooltip: {
+                trigger: 'item',
+                axisPointer: {
+                    type: 'shadow'
+                }
+            },
+            yAxis: {
+                type: 'value',
+                name: 'Triglicéridos',
+            },
+            series: [{
+                name: 'Grupo1',
+                type: 'line',
+                data:grupo_1_tag
+                },
+                {
+                name: 'Grupo2',
+                type: 'line',
+                data:grupo_2_tag,
+                lineStyle: {
+                    color: '#0f0'
+                    },
+                }
+            ]
+        };
+
+        var myChart = echarts.init(document.getElementById('chart_1'));
+        myChart.setOption(OptionChart1);
+
+        var myChart2 = echarts.init(document.getElementById('chart_2'));
+        myChart2.setOption(OptionChart2);
+
+        var myChart3 = echarts.init(document.getElementById('chart_3'));
+        myChart3.setOption(OptionChart3);
+
+        var myChart4 = echarts.init(document.getElementById('chart_4'));
+        myChart4.setOption(OptionChart4);
+    });
+
+
+//myChartGeo.showLoading();
