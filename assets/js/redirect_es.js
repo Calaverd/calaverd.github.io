ad8b347688103bd6b92f42f3aeb3bb5b0c008a8e
Calaverd <bfxdosval@gmail.com>
2023-04-30 00:34:26 -0600

    moving away from jekyll

--- assets/js/redirect_es.js
+++ assets/js/redirect_es.js
@@ -1,21 +1,17 @@
 // Si nos visitas usando un navegador en español, te llevamos al sitio en español.
 if (typeof(Storage) !== "undefined"){
-  if( !sessionStorage.set_lang ){
-    sessionStorage.set_lang = true;
-    // ¿la interfaz del navegador es en español?
-    if( window.navigator.language.search('es') != -1 ){
-      // console.log('El lenguaje del navegador es español'); 
-      if( window.location.href.search(/\/es\//) == -1 ){
-         // console.log("la pagina no esta en español, redirije a la versión en español");
-         let spanish_page = window.location.href.replace(/(?<=\/\/.*)\//,'/es/')
-         // ¿Exite la pagina en español?
-         fetch(spanish_page).then(function(response) {
-            console.log(response.status);
-            if(response.status == 200){
-               window.location.replace(spanish_page);
-                }
-            });
-       }
-    }
+  if( sessionStorage.set_lang ){
+    return;
   }
+  sessionStorage.set_lang = true;
+  // si el lenguaje del navegador no es español o la ventana ya esta en la version española
+  if( !window.navigator.language.includes('es') || window.location.href.includes("/es/") ){
+    return;
+  }
+  let spanish_page = window.location.href.replace(/(?<=\/\/.*)\//,'/es/');
+  fetch(spanish_page, {method: 'HEAD'}).then(response => {
+    if (response.status == 200) {
+        window.location.replace(spanish_page);
+      }
+  });
 }
